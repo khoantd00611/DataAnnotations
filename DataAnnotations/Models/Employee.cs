aba55e236c78f407ff5a97f094018e72f80908ef
khoantd00611 <khoantd00611@fpt.edu.vn>
2018-11-10 10:21:54 +0700

    ok

---
+++ DataAnnotations/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DataAnnotations.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Employee Name")]
+        [Required (ErrorMessage = "Employee Name is required")]
+        [StringLength(35)]
+
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Employee Name is required")]
+        [StringLength(300)]
+        public string Address { get; set; }
+        [Required (ErrorMessage ="Salary is required")]
+        [Range (30,1000000,ErrorMessage ="Salary must  be between 3000 and 1000000")]
+        public decimal Salary { get; set; }
+        [Required(ErrorMessage ="Please enter your email Address")]
+        [DataType(DataType.EmailAddress)]
+       
+        [MaxLength(50)]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}",ErrorMessage ="Please enter correct email address")]
+
+        public string Email { get; set; }
+    }
+}
